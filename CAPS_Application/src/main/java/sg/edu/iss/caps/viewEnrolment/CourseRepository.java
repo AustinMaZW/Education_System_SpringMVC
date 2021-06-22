f54c5f16f152601fe165544a3d5b1390f43732bb
Pengcheng Yue <79070290+wow7pm@users.noreply.github.com>
2021-06-23 04:16:34 +0800

    enrol form and list

--- CAPS_Application/src/main/java/sg/edu/iss/caps/viewEnrolment/CourseRepository.java
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/viewEnrolment/CourseRepository.java
@@ -1,9 +1,12 @@
 package sg.edu.iss.caps.viewEnrolment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.edu.iss.caps.model.Course;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
-
+	@Query("select c from Course c where c.name = :name")
+	Course findCourseByName(@Param("name") String name);
 }
--- CAPS_Application/src/main/java/sg/edu/iss/caps/viewEnrolment/EnrolmentController.java
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/viewEnrolment/EnrolmentController.java
@@ -1,12 +1,18 @@
 package sg.edu.iss.caps.viewEnrolment;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import sg.edu.iss.caps.model.Course;
 import sg.edu.iss.caps.model.CourseEnrolment;
 
 @Controller
@@ -14,12 +20,33 @@ import sg.edu.iss.caps.model.CourseEnrolment;
 public class EnrolmentController {
 	@Autowired
 	EnrolmentServiceImpl eservice;
+	@Autowired
+	CourseRepository crepo;
 
 	@RequestMapping("/list")
 	public String Enrolments(Model model) {
 		List<CourseEnrolment> list = eservice.findAllEnrolment();
 		model.addAttribute("enrols", list);
 		return "enrolments";
+	}
 
+	@RequestMapping("/add")
+	public String addEnrolment(Model model) {
+		model.addAttribute("enrol", new CourseEnrolment());
+		ArrayList<Course> courses = (ArrayList<Course>) crepo.findAll();
+		model.addAttribute("courses", courses);
+		return "Enrolment-form";
+	}
+
+	@RequestMapping("/save")
+	public String save(@ModelAttribute("enrol") @Valid CourseEnrolment enrol, BindingResult bindingResult,
+			Model model) {
+		if (bindingResult.hasErrors())
+			return "Enrolment-form";
+		Course course = crepo.findCourseByName(enrol.getCourse().getName());
+		if (eservice.CreateEnrolment(new CourseEnrolment(course, enrol.getStartDate(), enrol.getEndDate(),
+				enrol.getCapacity(), enrol.getStatus())))
+			return "forward:/enrolment/list";
+		return "forward:/enrolment/list";
 	}
 }
