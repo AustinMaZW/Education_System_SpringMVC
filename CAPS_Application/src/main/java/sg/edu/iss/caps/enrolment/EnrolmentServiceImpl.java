b3fcf2fbecd8a49a6f5ca313357c990f176af723
Pengcheng Yue <79070290+wow7pm@users.noreply.github.com>
2021-06-24 01:13:21 +0800

    following changes

--- CAPS_Application/src/main/java/sg/edu/iss/caps/enrolment/EnrolmentServiceImpl.java
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/enrolment/EnrolmentServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import sg.edu.iss.caps.model.Course;
