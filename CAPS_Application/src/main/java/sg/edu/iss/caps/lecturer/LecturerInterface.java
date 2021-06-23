ecd9172839a1c5eb51e83686c24666bb2fba08c3
TCK285 <cktang89@gmail.com>
2021-06-23 21:27:54 +0800

    rectified lecturer findbyname to findbyfirstname

--- CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/LecturerInterface.java
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/LecturerInterface.java
@@ -6,7 +6,7 @@ import sg.edu.iss.caps.model.Lecturer;
 
 public interface LecturerInterface {
 	public void createLecturer(Lecturer lecturer);
-	public Lecturer findLecturerByName(String name);
+	public Lecturer findLecturerByFirstName(String name);
 	public List<Lecturer> findAllLecturer();
 	public void updateLecturer(Lecturer lecturer);
 	public void deleteLecturer(Lecturer lecturer);
