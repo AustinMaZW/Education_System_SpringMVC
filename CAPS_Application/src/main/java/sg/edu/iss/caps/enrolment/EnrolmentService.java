f62526266cb3b18dd33d2449c038b752ed4b3626
Pengcheng Yue <79070290+wow7pm@users.noreply.github.com>
2021-06-24 01:11:05 +0800

    part of enrol because i cannot fix my comflicts, i need to manully merge somethings and re-import again

---
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/enrolment/EnrolmentService.java
@@ -0,0 +1,32 @@
+package sg.edu.iss.caps.enrolment;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import sg.edu.iss.caps.model.Course;
+import sg.edu.iss.caps.model.CourseEnrolment;
+import sg.edu.iss.caps.model.Student;
+
+public interface EnrolmentService {
+    List<CourseEnrolment> findAllEnrolment();
+
+    List<CourseEnrolment> findEnrolmentByStartDate(LocalDate start);
+
+    List<CourseEnrolment> findEnrolmentByEndDate(LocalDate end);
+
+    List<CourseEnrolment> findEnrolmentByDuration(LocalDate start, LocalDate end);
+
+    List<CourseEnrolment> findEnrolmentByCourse(Course course);
+
+    List<CourseEnrolment> findEnrolmentByStudent(Student student);
+
+    CourseEnrolment findEnrolmentById(int id);
+
+    boolean cancelEnrol(int id);
+
+    boolean UpdateEnrolment(CourseEnrolment enrol);
+
+    boolean CreateEnrolment(CourseEnrolment enrol);
+
+    void DeleteEnrolment(int id);
+}
