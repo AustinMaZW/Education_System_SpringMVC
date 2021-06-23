1adb37f720f428ad4ed6940f56597fceb141a342
TCK285 <78338297+TCK285@users.noreply.github.com>
2021-06-23 17:52:26 +0800

    Lecturer package added (#25)

---
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/LecturerController.java
@@ -0,0 +1,8 @@
+package sg.edu.iss.caps.lecturer;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LecturerController {
+	
+}
---
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/LecturerImplementation.java
@@ -0,0 +1,47 @@
+package sg.edu.iss.caps.lecturer;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sg.edu.iss.caps.model.Lecturer;
+
+@Service
+public class LecturerImplementation implements LecturerInterface {
+
+	@Autowired
+	LecturerRepository lrepo;
+	
+	@Override
+	public void createLecturer(Lecturer lecturer) {
+		// TODO Auto-generated method stub
+		lrepo.save(lecturer);
+	}
+
+	@Override
+	public Lecturer findLecturerByName(String name) {
+		// TODO Auto-generated method stub
+		return lrepo.findLecturerByName(name);
+	}
+
+	@Override
+	public List<Lecturer> findAllLecturer() {
+		// TODO Auto-generated method stub
+		return lrepo.findAll();
+	}
+
+	@Override
+	public void updateLecturer(Lecturer lecturer) {
+		// TODO Auto-generated method stub
+		lrepo.save(lecturer);
+	}
+
+	@Override
+	public void deleteLecturer(Lecturer lecturer) {
+		// TODO Auto-generated method stub
+		lrepo.delete(lecturer);
+	}
+
+
+}
