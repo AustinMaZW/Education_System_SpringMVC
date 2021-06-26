0c7912afb173c2d8f0ce75ed4d4e5b3c159dc28b
Pengcheng Yue <79070290+wow7pm@users.noreply.github.com>
2021-06-26 23:39:17 +0800

    view enrolment for enrol version 2

--- CAPS_Application/src/main/java/sg/edu/iss/caps/enrolment/ViewCourseEnrolController.java
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/enrolment/ViewCourseEnrolController.java
@@ -9,37 +9,63 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import sg.edu.iss.caps.model.CourseEnrolment;
 import sg.edu.iss.caps.model.Student;
-import sg.edu.iss.caps.student.StudentServiceImpl;
+import sg.edu.iss.caps.student.StudentService;
 
 @Controller
 @RequestMapping("/courseview")
 public class ViewCourseEnrolController {
 	@Autowired
-	EnrolmentServiceImpl eservice;
+	EnrolmentService eservice;
 	@Autowired
-	StudentServiceImpl sservice;
+	StudentService sservice;
 
 	@RequestMapping("/list")
 	public String viewList(Model model) {
 		String name = userName();
 		Student stu = sservice.findStudentByUsername(name);
 		Map<CourseEnrolment, Double> grades = stu.getGrades();
-		List<CourseEnrolment> enrols = new ArrayList<CourseEnrolment>(stu.getGrades().keySet());
-		List<CourseEnrolment> allEnrols = eservice.findAllEnrolment();
-		List<CourseEnrolment> validEnrols = new ArrayList<>();
-		allEnrols.stream().forEach(x -> {
-			enrols.stream().forEach(y -> {
-				if (x.getCourse().getName().compareTo(y.getCourse().getName()) == 0)
-					validEnrols.add(x);
-			});
-		});
-		model.addAttribute("validEnrols", validEnrols);
 		model.addAttribute("grades", grades);
 		return "course-enrol-list";
+	}
+
+	@RequestMapping("/list/enrol")
+	public String enrolList(Model model) {
+		String name = userName();
+		Student stu = sservice.findStudentByUsername(name);
+		List<CourseEnrolment> enrols = new ArrayList<CourseEnrolment>(stu.getGrades().keySet());
+		List<CourseEnrolment> allEnrols = eservice.findAllEnrolment();
+		enrols.stream().forEach(x -> {
+			if (allEnrols.contains(x)) {
+				allEnrols.remove(x);
+			}
+		});
+		model.addAttribute("validEnrol", allEnrols);
+		return "enrolList";
+	}
+
+	@RequestMapping("/add/{id}")
+	public String takeEnrol(@PathVariable("id") int id, Model model) {
+		String name = userName();
+		Student stu = sservice.findStudentByUsername(name);
+		CourseEnrolment newEnrol = eservice.findEnrolmentById(id);
+		Map<CourseEnrolment, Double> newE = stu.getGrades();
+		sservice.setEnrol(newEnrol, stu);
+		return "redirect:/courseview/list";
+	}
+
+	@RequestMapping("/cancel/{id}")
+	public String cancel(@PathVariable("id") int id) {
+		String name = userName();
+		Student stu = sservice.findStudentByUsername(name);
+		CourseEnrolment enrol = eservice.findEnrolmentById(id);
+		sservice.cancel(stu, enrol);
+		return "redirect:/courseview/list";
+
 	}
 
 	public String userName() {
