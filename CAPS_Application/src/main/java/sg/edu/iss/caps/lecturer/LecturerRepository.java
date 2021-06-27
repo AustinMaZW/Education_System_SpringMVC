781acec44e631ba67d5a8e77378da8cadaa93b20
TCK285 <cktang89@gmail.com>
2021-06-27 14:04:32 +0800

    update CK branch

--- CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/LecturerRepository.java
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/LecturerRepository.java
@@ -13,8 +13,7 @@ public interface LecturerRepository extends JpaRepository<Lecturer, Integer>{
 	Lecturer findByUsername(String username);
 	
 	Lecturer findLecturerByFirstName(String firstName);
-	
-	Lecturer findLecturerById(int id);
+
 	
 	@Query ("SELECT l FROM Lecturer l where l.firstName = :lfn and l.lastName = :lln")
 	Lecturer findLecturer(@Param("lfn") String lfn, @Param("lln") String lln);
