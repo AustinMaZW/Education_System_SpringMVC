8e79f1c7dd99413acee2381936dd579a07895907
AustinMaZW <auzzyma@gmail.com>
2021-06-29 20:21:49 +0800

    Test Code for Manage Grade - Lecturer

--- CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/ViewCourseTaughtController.java
+++ CAPS_Application/src/main/java/sg/edu/iss/caps/lecturer/ViewCourseTaughtController.java
@@ -9,7 +9,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import sg.edu.iss.caps.course.Course;
@@ -56,5 +58,23 @@ public class ViewCourseTaughtController {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		String name = auth.getName();
 		this.lecturer = lservice.findLecturerByUsername(name);
+	}
+	
+	//Austin test code for manage grades 
+	@RequestMapping("/enrol/students/{id}")
+	public String studentList(@PathVariable("id") int id, Model model) {
+		CourseEnrolment courseEnrolment = eservice.findEnrolmentById(id);
+		List<Student> studentList= eservice.findStudentsByEnrol(courseEnrolment);
+		model.addAttribute("students", studentList);
+		model.addAttribute("enrol", courseEnrolment);
+		return "/lecturer/ViewStudentEnrolled_Austin";
+	}
+	
+	@PostMapping("/enrol/savegrades")
+	public String saveGrades(@ModelAttribute("Student") List<Student> studentList) {
+		for(Student s: studentList) {
+			System.out.println(s.getId());
+		}
+		return "redirect:/lecture/list";
 	}
 }
